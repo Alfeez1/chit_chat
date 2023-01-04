4c9a26cbbbdd7596b7f4712b6f6477bf45bb981a
Alfeez1 <Muhammed.alfeez@b-jelt.com>
2023-01-04 13:00:28 +0530

    url changed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use(
   cors({
-    origin: ['http://localhost:5000', 'https://chit-chat-server.onrender.com'],
+    origin: ['https://chit-chat-client.onrender.com'],
     credentials: true,
   })
 );
@@ -67,7 +67,10 @@ io.on('connection', (socket) => {
 });
 app.use(express.static(path.join(__dirname, '../public/build')));
 app.get('*', (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.set(
+    'Access-Control-Allow-Origin',
+    'https://chit-chat-client.onrender.com'
+  );
   res.sendFile(path.join(__dirname, '../public/build/index.html'));
 });
 // app.get('/', (req, res) => {
